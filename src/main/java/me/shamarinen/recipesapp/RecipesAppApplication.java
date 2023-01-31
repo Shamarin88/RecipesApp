d34b3563c6ff2abe5874efe0f64e3b7b9bc7b296
Shamarin88 <shamarin77@gmail.com>
2023-01-31 19:50:52 +0300

    Начальный коммит

--- src/main/java/me/shamarinen/recipesapp/RecipesAppApplication.java
+++ src/main/java/me/shamarinen/recipesapp/RecipesAppApplication.java
@@ -10,4 +10,5 @@ public class RecipesAppApplication {
         SpringApplication.run(RecipesAppApplication.class, args);
     }
 
+
 }
--- src/main/java/me/shamarinen/recipesapp/controllers/FirstController.java
+++ src/main/java/me/shamarinen/recipesapp/controllers/FirstController.java
@@ -1,4 +1,5 @@
 package me.shamarinen.recipesapp.controllers;
 
 public class FirstController {
+
 }
